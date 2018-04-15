363d9f9a351265a211a04e01fe080957020082e6
conradoramalho <conrado.ramalho94@gmail.com>
2018-04-15 15:53:42 -0300

    refactor: change functions to arrow functions in actions

--- react-app/src/app/actions/category-actions.js
+++ react-app/src/app/actions/category-actions.js
@@ -1,20 +1,16 @@
-export const CATEGORY_REQUEST = 'CATEGORY_REQUEST';
-export const CATEGORY_REQUEST_SUCCESS = 'CATEGORY_SUCCESS';
-export const CATEGORY_REQUEST_FAILURE = 'CATEGORY_FAILURE';
+export const CATEGORY_REQUEST = "CATEGORY_REQUEST";
+export const CATEGORY_REQUEST_SUCCESS = "CATEGORY_SUCCESS";
+export const CATEGORY_REQUEST_FAILURE = "CATEGORY_FAILURE";
 
-export const POST_CATEGORY_LIST_REQUEST = 'POST_CATEGORY_LIST_REQUEST'
-export const POST_CATEGORY_LIST_SUCCESS = 'POST_CATEGORY_LIST_SUCCESS'
-export const POST_CATEGORY_LIST_FAILURE = 'POST_CATEGORY_LIST_FAILURE'
+export const POST_CATEGORY_LIST_REQUEST = "POST_CATEGORY_LIST_REQUEST";
+export const POST_CATEGORY_LIST_SUCCESS = "POST_CATEGORY_LIST_SUCCESS";
+export const POST_CATEGORY_LIST_FAILURE = "POST_CATEGORY_LIST_FAILURE";
 
-export function getCategoryList() {
-    return {
-        type: CATEGORY_REQUEST
-    };
-}
+export const getCategoryList = () => ({
+  type: CATEGORY_REQUEST
+});
 
-export function getPostCategory(category) {
-    return {
-        type: POST_CATEGORY_LIST_REQUEST,
-        payload: category
-    };
-}
+export const getPostCategory = category => ({
+  type: POST_CATEGORY_LIST_REQUEST,
+  payload: category
+});
--- react-app/src/app/actions/comment-actions.js
+++ react-app/src/app/actions/comment-actions.js
@@ -1,32 +1,28 @@
-export const COMMENTS_REQUEST = 'COMMENTS_REQUEST';
-export const COMMENTS_REQUEST_SUCCESS = 'COMMENTS_SUCCESS';
-export const COMMENTS_REQUEST_FAILURE = 'COMMENTS_FAILURE';
+export const COMMENTS_REQUEST = "COMMENTS_REQUEST";
+export const COMMENTS_REQUEST_SUCCESS = "COMMENTS_SUCCESS";
+export const COMMENTS_REQUEST_FAILURE = "COMMENTS_FAILURE";
 
-export const COMMENTS_NEW_REQUEST = 'COMMENTS_NEW_REQUEST';
-export const COMMENTS_NEW_REQUEST_SUCCESS = 'COMMENTS_NEW_REQUEST_SUCCESS';
-export const COMMENTS_NEW_REQUEST_FAILURE = 'COMMENTS_NEW_REQUEST_FAILURE';
+export const COMMENTS_NEW_REQUEST = "COMMENTS_NEW_REQUEST";
+export const COMMENTS_NEW_REQUEST_SUCCESS = "COMMENTS_NEW_REQUEST_SUCCESS";
+export const COMMENTS_NEW_REQUEST_FAILURE = "COMMENTS_NEW_REQUEST_FAILURE";
 
-export const UPDATE_COMMENTS_REQUEST = 'UPDATE_COMMENTS_REQUEST';
-export const UPDATE_COMMENTS_REQUEST_SUCCESS = 'UPDATE_COMMENTS_REQUEST_SUCCESS';
-export const UPDATE_COMMENTS_REQUEST_FAILURE = 'UPDATE_COMMENTS_REQUEST_FAILURE';
+export const UPDATE_COMMENTS_REQUEST = "UPDATE_COMMENTS_REQUEST";
+export const UPDATE_COMMENTS_REQUEST_SUCCESS =
+  "UPDATE_COMMENTS_REQUEST_SUCCESS";
+export const UPDATE_COMMENTS_REQUEST_FAILURE =
+  "UPDATE_COMMENTS_REQUEST_FAILURE";
 
-export function getCommentsByPostId(params) {
-    return {
-        type: COMMENTS_REQUEST,
-        payload: params
-    };
-}
+export const getCommentsByPostId = params => ({
+  type: COMMENTS_REQUEST,
+  payload: params
+});
 
-export function sendNewComment(params) {
-    return {
-        type: COMMENTS_NEW_REQUEST,
-        payload: params
-    };
-}
+export const sendNewComment = params => ({
+  type: COMMENTS_NEW_REQUEST,
+  payload: params
+});
 
-export function updateComment(params) {
-    return {
-        type: UPDATE_COMMENTS_REQUEST,
-        payload: params
-    };
-}
+export const updateComment = params => ({
+  type: UPDATE_COMMENTS_REQUEST,
+  payload: params
+});
